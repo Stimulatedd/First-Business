2a89b6d8c9bfffe6fd60e4139579d749cb4f1aaa
Stimulatedd <brandenkeiffer@gmail.com>
2024-10-03 19:36:58 -0700

    Update tweet_bot.py

--- tweet_bot.py
+++ tweet_bot.py
@@ -1,10 +1,10 @@
 import tweepy
 
 # Your API credentials
-consumer_key = 'your_consumer_key'
-consumer_secret = 'your_consumer_secret'
-access_token = 'your_access_token'
-access_token_secret = 'your_access_token_secret'
+consumer_key = 'Le8ncsi5FS7MFOuZN0l7WEiOf'
+consumer_secret = 'chlQbS0221qf0WX4LWheZT7fVuUtmzn87UMjis8WZ4hDCgskKl'
+access_token = '1471604210806312960-EDexpTgBqHbyuudLDCnpwavZy79t4Y'
+access_token_secret = 'n2X4RPKJM6Wq78z8cl8hN5kXJ4wo9RozhvNx837JdTjCB'
 
 # Authenticate to Twitter
 auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
